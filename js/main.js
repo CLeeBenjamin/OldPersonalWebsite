8661223e77b1b9b8fc6a11f76679c6c811d4e4bb
CLeeBenjamin <33280446+CLeeBenjamin@users.noreply.github.com>
2018-06-20 22:13:27 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -147,18 +147,17 @@
     });
     
     //Skill
-//     jQuery('.skillbar').each(function() {
-//       jQuery(this).appear(function() {
-//         jQuery(this).find('.count-bar').animate({
-//           width:jQuery(this).attr('data-percent')
-//         },3000);
-//         var percent = jQuery(this).attr('data-percent');
-//         jQuery(this).find('.count').html('<span>' + percent + '</span>');
-//       });
-//     }); 
+    jQuery('.skillbar').each(function() {
+       jQuery(this).appear(function() {
+         jQuery(this).find('.count-bar').animate({
+           width:jQuery(this).attr('data-percent')
+         },3000);
+         var percent = jQuery(this).attr('data-percent');
+         jQuery(this).find('.count').html('<span>' + percent + '</span>');
+       });
+    }); 
 
-  
-//   });
+     });
   
     
   //header
